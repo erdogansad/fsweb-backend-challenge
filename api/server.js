55530331aa612b3735e0fe4b0ee3977c17e0f0a3
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-07-10 14:23:26 +0300

    initial server

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const server = express();
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+const router = require("./router/router.js");
+const { errHandler } = require("./middlewares.js");
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(morgan("combined"));
+server.use("/api", router);
+server.use(errHandler);
+
+module.exports = server;
