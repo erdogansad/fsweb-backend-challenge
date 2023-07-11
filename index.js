926c87b40032cbd8b0c212bad29dc70361d1cf13
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-07-12 00:28:43 +0300

    dotenv called

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = require("./api/server.js");
 require("dotenv").config();
+const app = require("./api/server.js");
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`\n== API Running on port ${port} ==\n`));
