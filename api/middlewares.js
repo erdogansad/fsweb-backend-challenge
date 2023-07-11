b9aa42fde8c7b2e81ca5975ddf147e644bed8f65
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-07-12 00:44:52 +0300

    token controller added

--- api/middlewares.js
+++ api/middlewares.js
@@ -1,5 +1,28 @@
+const db = require("../data/config");
+
 const errHandler = (err, req, res, next) => {
   res.headersSent ? next(err) : res.status(err.status || 500).json({ message: err.message || "internal server error." });
 };
 
-module.exports = { errHandler };
+const tokenController = async (req, res, next) => {
+  try {
+    let token = req.headers.authorization;
+    if (token) {
+      token = token.split(" ")[1];
+      const tokenValid = await db("tokens").where({ token }).first(),
+        compareDates = tokenValid ? new Date(tokenValid.expires_at) > new Date() : false;
+      if (tokenValid && compareDates) {
+        req.token = tokenValid;
+        next();
+      } else {
+        next({ status: 401, message: "token invalid." });
+      }
+    } else {
+      next({ status: 403, message: "token required." });
+    }
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports = { errHandler, tokenController };
--- api/router/router.js
+++ api/router/router.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const auth = require("./routes/auth/auth-router.js");
 const tweets = require("./routes/tweets/tweets-router.js");
+const { tokenController } = require("../middlewares.js");
 
 router.use("/auth", auth);
-router.use("/tweets", tweets);
+router.use("/tweets", tokenController, tweets);
 
 module.exports = router;
