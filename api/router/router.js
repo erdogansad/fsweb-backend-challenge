97a7fe8ce696bfdb030e42cc65991f0c3e270f26
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-07-12 00:29:13 +0300

    tweets route added

--- api/router/router.js
+++ api/router/router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const auth = require("./routes/auth/auth-router.js");
+const tweets = require("./routes/tweets/tweets-router.js");
 
 router.use("/auth", auth);
+router.use("/tweets", tweets);
 
 module.exports = router;
