087d7aa2f2037a8c3592bb558b3f42289bf698cf
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-07-18 15:15:19 +0300

    index route added

--- api/server.js
+++ api/server.js
@@ -10,6 +10,9 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use(morgan("combined"));
+server.all("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the API" });
+});
 server.use("/api", router);
 server.use(errHandler);
 
