072c04c4cbb43b2d34917b688638b4c96f232e59
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-07-18 15:03:47 +0300

    2 routes added

--- api/router/routes/users/users-modal.js
+++ api/router/routes/users/users-modal.js
@@ -12,4 +12,20 @@ const getById = (id) => {
     .first();
 };
 
-module.exports = { getAll, getById };
+const getLikes = (id) => {
+  return db
+    .select("tw.tweet_id", "tw.user_id", "tw.tweet", "tw.created_at", "li.like_id")
+    .from("tweets as tw")
+    .join("likes as li", "li.tweet_id", "tw.tweet_id")
+    .where("li.user_id", id);
+};
+
+const getMentions = (id) => {
+  return db
+    .select("tw.tweet_id", "tw.user_id", "tw.tweet", "tw.created_at", "me.mention_id", "me.mention", "me.created_at as mention_created_at")
+    .from("tweets as tw")
+    .join("mentions as me", "me.tweet_id", "tw.tweet_id")
+    .where("me.user_id", id);
+};
+
+module.exports = { getAll, getById, getLikes, getMentions };
--- api/router/routes/users/users-router.js
+++ api/router/routes/users/users-router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { getAll } = require("./users-modal.js");
+const { getAll, getLikes, getMentions } = require("./users-modal.js");
 const { getByUserId } = require("#tweets/tweets-modal.js");
 const { userController } = require("./users-middlewares.js");
 
@@ -23,6 +23,24 @@ router.get("/:id", userController, async (req, res, next) => {
 router.get("/:id/tweets", userController, async (req, res, next) => {
   try {
     const query = await getByUserId(req.params.id);
+    res.status(200).json(query);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id/likes", userController, async (req, res, next) => {
+  try {
+    const query = await getLikes(req.params.id);
+    res.status(200).json(query);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id/mentions", userController, async (req, res, next) => {
+  try {
+    const query = await getMentions(req.params.id);
     res.status(200).json(query);
   } catch (e) {
     next(e);
