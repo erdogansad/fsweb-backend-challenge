0c7565d19a1a47c5e32c6d09a8f9eaed245694c3
erdogansad <60552846+erdogansad@users.noreply.github.com>
2023-07-19 17:54:08 +0300

    query edited

--- api/router/routes/users/users-modal.js
+++ api/router/routes/users/users-modal.js
@@ -1,15 +1,11 @@
 const db = require("#data/config.js");
 
 const getAll = () => {
-  return db.select("us.user_id", "us.email", "us.username", "us.first_name", "us.last_name", "us.avatar").from("users as us");
+  return db.select("user_id", "email", "username", "first_name", "last_name", "avatar").from("users");
 };
 
 const getById = (id) => {
-  return db
-    .select("us.user_id", "us.email", "us.username", "us.first_name", "us.last_name", "us.avatar", "us.created_at")
-    .from("users as us")
-    .where("us.user_id", id)
-    .first();
+  return db.select("user_id", "email", "username", "first_name", "last_name", "avatar", "created_at").from("users").where("user_id", id).first();
 };
 
 const getLikes = (id) => {
